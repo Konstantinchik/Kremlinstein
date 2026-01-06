9168e5eaac1a45f0ba71ef25481b49c8b41a7e01
Konstantinchik <kgluschenko83@gmail.com>
2026-01-07 00:58:24 +0300

    Fix skin

--- Assets/Game/Scripts/MenuScripts/MainMenuController.cs
+++ Assets/Game/Scripts/MenuScripts/MainMenuController.cs
@@ -18,7 +18,7 @@ public class MainMenuController : MonoBehaviour
         clickSound.Play();
         fadeOut.SetActive(true);
         yield return new WaitForSeconds(2);
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
